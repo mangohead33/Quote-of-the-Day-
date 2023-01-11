04d36365fc0d67c707d4e3d0f7a5ec0496dac7d8
Calvin Chau <cachau33@gmail.com>
2023-01-11 11:49:28 -0800

    Few Changes: Moved quotes outside of App and is now a global variable. When quoteId state changes, favStatus is also updated as well. AdjustFav function now properly toggles the favStatus state. While app is running, program will retain info on which quotes have been modified.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Textbox from './components/Textbox.js';
 
+// quote = ["quote", favorite: boolean]
+let quotes = [
+  ['to die or not to die', false],
+  ['better in than out as i always say', false],
+  ['hey wheres the beef', false],
+  ['cowabunga', false],
+  [`i'm so hungry`, false],
+  [`don't have a cow man`, false]
+];
+
 const App = () => {
-  const quotes = [
-    'to die or not to die',
-    'better in than out as i always say',
-    'hey wheres the beef',
-    'copwabunga',
-    `i'm so hungry`,
-    `don't have a cow man`
-  ]
-  let quote = quotes[Math.floor(Math.random() * quotes.length)];
+  let [quoteId, setQuoteId] = useState(Math.floor(Math.random() * quotes.length))
+  let [favStatus, setFavStatus] = useState(quotes[quoteId][1]);
+
+  useEffect(() => {
+    setFavStatus(quotes[quoteId][1]);
+  }, [quoteId])
+
+  function adjustQuoteId() {
+    //setQuoteId(Math.floor(Math.random() * quotes.length));
+    setQuoteId(() => Math.floor(Math.random() * quotes.length));
+  }
+
+  function adjustFav() {
+    setFavStatus((c) => {
+      quotes[quoteId][1] = !c;
+      return !c;
+    })
+  }
+
   return (
-      <Textbox quote ={quote}/>
+      <Textbox
+      quote ={quotes[quoteId]}
+      favStatus={favStatus}
+      shuffleQuote={() => adjustQuoteId()}
+      favoriteAQuote={() => adjustFav()}
+      />
   )
 }
 
-export default App
+export default App;
