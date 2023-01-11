f68c29774d4e760fb68db12024434487be7b3497
Calvin Chau <cachau33@gmail.com>
2023-01-11 14:15:21 -0800

    Imported style sheet, added class to div containers for easier styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import Textbox from './components/Textbox.js';
+import './components/styles/style.css'
 
 // quote = ["quote", favorite: boolean]
 let quotes = [
-  ['to die or not to die', false],
-  ['better in than out as i always say', false],
-  ['hey wheres the beef', false],
-  ['cowabunga', false],
-  [`i'm so hungry`, false],
-  [`don't have a cow man`, false]
+  ['Be yourself; everyone else is already taken.', false],
+  [`Two things are infinite: the universe and human stupidity; and I'm not sure about the universe`, false],
+  ['So many books, so little time.', false],
+  [`Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.`, false],
+  [`You know you're in love when you can't fall asleep because reality is finally better than your dreams.`, false],
+  [`If you tell the truth, you don't have to remember anything.`, false],
+  [`I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.`, false],
+  [`Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.`, false]
 ];
 
 const App = () => {
@@ -31,12 +34,15 @@ const App = () => {
   }
 
   return (
-      <Textbox
-      quote ={quotes[quoteId]}
-      favStatus={favStatus}
-      shuffleQuote={() => adjustQuoteId()}
-      favoriteAQuote={() => adjustFav()}
-      />
+    <>
+    <h1>Quote of The Day</h1>
+    <Textbox
+    quote ={quotes[quoteId]}
+    favStatus={favStatus}
+    shuffleQuote={() => adjustQuoteId()}
+    favoriteAQuote={() => adjustFav()}
+    />
+    </>
   )
 }
 
--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -1,14 +1,17 @@
 import React, {useState} from 'react';
 import ShuffleQuoteButton from './ShuffleQuoteButton.js';
 import FavoritesButton from './FavoritesButton.js';
+import './styles/style.css'
 
 function Textbox({quote, shuffleQuote, favStatus, favoriteAQuote}) {
   return (
-    <div>
-  <div>{quote[0]}</div>
+    <>
+  <div class="quote-style">{quote[0]}</div>
+  <div class="button-container">
   <FavoritesButton favStatus={favStatus} favoriteAQuote={favoriteAQuote}/>
   <ShuffleQuoteButton shuffleQuote={shuffleQuote}/>
   </div>
+  </>
   );
 }
 export default Textbox;
